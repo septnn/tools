c7f485d1cedefe74b02cefc2f1d19ac456d516de
dawei <dawei@juewei.com>
2019-12-17 09:19:02 +0800

    同步代码

--- python/fang/fang/fang/spiders/beike.py
+++ python/fang/fang/fang/spiders/beike.py
@@ -1,8 +1,10 @@
 import scrapy;
-import fang.spiders.fangDb as fangDb;
+from fang.spiders.fangDb import *
 
 class beike(scrapy.Spider):
     name = "beike"
+    def __init__(self):
+        self.fangDb = fangDb()
 
     def start_requests(self):
         urls = [];
@@ -21,41 +23,27 @@ class beike(scrapy.Spider):
             'https://tj.ke.com/ershoufang/19121411410100117140.html'
         ]
         for url in urls:
-            # fangDb.insertUrl(url)
-            self.detail(url)
+            # self.fangDb.insertUrl(url)
+            yield scrapy.Request(url=url, callback=self.detailCallback)
 
-        # filename = 'quotes.html'
-        # with open(filename, 'wb') as f:
-        #     f.write(response.body)
-        # self.log('Saved file %s ' % filename)
-    def detail(self, url):
-        scrapy.Request(url=url, callback=self.parse)
     def detailCallback(self, response):
         fkey = response.xpath('//head/title/text()').get()
         name = response.xpath('//h1[@class="main"]/@title').get()
         total = response.xpath('//span[@class="total"]/text()').get()
-        unit = response.xpath('//span[@class="unitPrice"]/text()').get()
+        unit = response.xpath('//span[@class="unitPriceValue"]/text()').get()
         house_loyout = response.xpath('//div[@class="room"]/div[@class="mainInfo"]/text()').get()
         house_turn = response.xpath('//div[@class="type"]/div[@class="mainInfo"]/text()').get()
         house_area = response.xpath('//div[@class="area"]/div[@class="mainInfo"]/text()').get()
         house_build = response.xpath('//div[@class="area"]/div[@class="subInfo"]/text()').get()
         community = response.xpath('//div[@class="communityName"]/a[@class="info no_resblock_a"]/text()').get()
-        area = response.xpath('//div[@class="areaName"]/a"]/text()').getAll() # 多个
+        area = response.xpath('//div[@class="areaName"]').getall() # 多个
         base_detail = response.xpath('//div[@class="base"]/div[@class="content"]/ul/li').getall() # 多个
         transaction = response.xpath('//div[@class="transaction"]/div[@class="content"]/ul/li').getall() # 多个
         special = response.xpath('//div[@class="introContent showbasemore"]//a/text()').getall() # 多个
         house_img = response.xpath('//div[@class="thumbnail"]//li[@data-desc="户型图"]/@data-pic').get()
-        fangDb.insertDetail(fkey,
-                            name,
-                            total,
-                            unit,
-                            house_loyout,
-                            house_turn,
-                            house_area,
-                            house_build,
-                            community,
-                            area,
-                            base_detail,
-                            transaction,
-                            special,
-                            house_img)
+        self.fangDb.insertDetail(fkey, name, total, unit, house_loyout, house_turn, house_area, house_build, community, area, base_detail, transaction, special, house_img)
+
+        # filename = 'quotes.html'
+        # with open(filename, 'wb') as f:
+        #     f.write(response.body)
+        # self.log('Saved file %s ' % filename)
