64ff0aa76b0c7fd970c52d5c178e752d38d0fac0
wei.da <wei.da@vhall.com>
2020-12-30 10:11:57 +0800

    更新v3.1开发配置

--- docker/dev/v3.1/conf/php7.3.25/build.sh
+++ docker/dev/v3.1/conf/php7.3.25/build.sh
@@ -1,5 +1,5 @@
-docker stop phpfpm7033
-docker rm phpfpm7033
-docker rmi $(docker images phpfpm7033 -q)
-docker build -t phpfpm7033 .
-docker run --name phpfpm7033 -itd --network vhall --network-alias phpfpm7033 --cap-add=SYS_PTRACE  -v /mnt/e/vhall:/home/vhall -v /mnt/e/github/tools/docker/dev/v3.1/conf/php7.0.33:/usr/local/etc phpfpm7033
+docker stop phpfpm7325
+docker rm phpfpm7325
+docker rmi $(docker images phpfpm7325 -q)
+docker build -t phpfpm7325 .
+docker run --name phpfpm7325 -itd --network vhall --network-alias phpfpm7325 --cap-add=SYS_PTRACE  -v /mnt/e/vhall:/home/vhall -v /mnt/e/github/tools/docker/dev/v3.1/conf/php7.3.25:/usr/local/etc phpfpm7325
